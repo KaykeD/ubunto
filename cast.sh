ced52dfdc361bfed286f8c61599f2677181c40b2
KaykeD <44686044+KaykeD@users.noreply.github.com>
2018-11-01 20:09:40 -0300

    Create cast.sh

---
+++ cast.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+#meu primeiro script
+
+#listando os diretorios do linux
+ls -lha /
+read
+clear
+
+#atulizando o apt ou apt-get 
+apt update
+echo "listas atualizadas"
+read
+cls
+
+#desligando o sercvidor
+shutdown -h now
